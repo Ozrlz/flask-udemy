249783d7f69188df81fddbbfacefa1a8011376d7
ozrlz <oscar.cucei@gmail.com>
2018-07-19 13:12:36 -0500

    [IMP] Add a @staticmethod and @classmethod decorator example to main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
-print ("Hello world from inside a container c:")
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+    @classmethod
+    def class_method(cls):
+        print ("This is a class method")
+
+        print ("I'm a {}".format(cls))
+
+    @staticmethod
+    def static_method():
+        print ("This is a static method")
+
+
+anna = Student('Anna', 'MIT')
+ozrlz = Student('ozrlz', 'UDG')
+
+ozrlz.class_method()
+Student.static_method()
