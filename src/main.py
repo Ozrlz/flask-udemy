073b1f432d3b3d5b9dd1bdf1adf3617da77f6287
ozrlz <oscar.cucei@gmail.com>
2018-07-20 11:10:38 -0500

    [IMP]Add the compute to the endpoints <3.43>

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from os import environ
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from pdb import set_trace as debug
 
 FLASK_PORT = environ.get('FLASK_PORT')
 
@@ -17,19 +18,27 @@ stores = [
     }
 ]
 
-# PORT - To retrieve data
-# GET - to send data back only
 
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data.get('name'),
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name): # The parameter must match the name on the decorator
-    return name
+    # Return the store, else an error msg
+    for store in stores:
+        if store.get('name') == name:
+            return jsonify(store)
+    return jsonify({'message': 'Store not found ):'})
 
 # GET /store
 @app.route('/store')
@@ -42,14 +51,24 @@ def get_stores():
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item')
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store.get('name') == name:
+            debug()
+            store.get('items').append(request_data)
+            # return jsonify({'message': 'The items were pushed xd'})
+            return jsonify(request_data)
+    return jsonify({'message': 'The store was not found ):'} )
 
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    pass
+def get_items_in_store(name):
+    for store in stores:
+        if store.get('name') == name:
+            return jsonify({'items': store.get('items')})
+    return jsonify({'message': 'The store was not found ):'})
 
 
-
-app.run(port=FLASK_PORT, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(port=FLASK_PORT, host='0.0.0.0')
