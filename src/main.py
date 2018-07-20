768ab30d0e78a50d4b27cad697f0cc99d163c608
ozrlz <oscar.cucei@gmail.com>
2018-07-20 10:34:12 -0500

    [IMP] Add the headers of the endpoints's methods

--- src/main.py
+++ src/main.py
@@ -2,11 +2,40 @@ from os import environ
 
 from flask import Flask
 
-app = Flask(__name__)
 FLASK_PORT = environ.get('FLASK_PORT')
+app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello world!"
+# PORT - To retrieve data
+# GET - to send data back only
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name): # The parameter must match the name on the decorator
+    return name
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item')
+def create_item_in_store(name):
+    pass
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+
+
 
 app.run(port=FLASK_PORT, host='0.0.0.0')
