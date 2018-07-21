81a44146da2507666dd26cd9bc42639ff6cfd3ab
ozrlz <oscar.cucei@gmail.com>
2018-07-21 00:24:41 -0500

    [IMP] Creation of first resource (item) with post and get methods.
    <4.52>

--- src/main.py
+++ src/main.py
@@ -9,11 +9,23 @@ FLASK_PORT = environ.get('FLASK_PORT')
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = [{
+
+}]
+
+class Item(Resource):
 
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item.get('name') == name:
+                return item
+        return {'message': 'item not found u.u'}, 404
+
+    def post(self, name):
+        item = {'name': name, 'price': 15.99}
+        items.append(item)
+        return item, 201
 
 if __name__ == '__main__':
-    api.add_resource(Student, '/student/<string:name>')
+    api.add_resource(Item, '/item/<string:name>')
     app.run(port=FLASK_PORT, host='0.0.0.0')
