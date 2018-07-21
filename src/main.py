4cd717726d9f28e82ee2baf1777d2361a6fc4101
ozrlz <oscar.cucei@gmail.com>
2018-07-21 00:43:33 -0500

    [IMP] Add a ItemList Resource and extend the post and get methods of the Item Resource.
    <4.53>

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from os import environ
 
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 from pdb import set_trace as debug
 
@@ -9,9 +9,7 @@ FLASK_PORT = environ.get('FLASK_PORT')
 app = Flask(__name__)
 api = Api(app)
 
-items = [{
-
-}]
+items = []
 
 class Item(Resource):
 
@@ -22,10 +20,20 @@ class Item(Resource):
         return {'message': 'item not found u.u'}, 404
 
     def post(self, name):
-        item = {'name': name, 'price': 15.99}
+        # Fails if does not have a payload or the ContentType header is not set
+        # Can be bypassed with the force flag, but always executes it
+        # Also, the silent flag can help not to rise an error
+        payload = request.get_json(silent=True)
+        item = {'name': name, 'price': payload.get('price')}
         items.append(item)
         return item, 201
 
+class ItemList(Resource):
+    
+    def get(self):
+        return {'items': items}
+
 if __name__ == '__main__':
     api.add_resource(Item, '/item/<string:name>')
+    api.add_resource(ItemList, '/items')
     app.run(port=FLASK_PORT, host='0.0.0.0')
