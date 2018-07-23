287b85e8c31dfbcb2b373d6f5c32c8aaae812030
ozrlz <oscar.cucei@gmail.com>
2018-07-22 19:23:10 -0500

    [IMP Add a DELETE http verb to the item Resource
    <4.57>

--- src/main.py
+++ src/main.py
@@ -35,6 +35,11 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x.get('name') != name, items))
+        return {'message': 'Item deleted'}
+
 class ItemList(Resource):
     
     def get(self):
