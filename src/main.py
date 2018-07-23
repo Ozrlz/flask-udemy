752127ee934b8213ebfd3e5f8552a744db13a72f
ozrlz <oscar.cucei@gmail.com>
2018-07-22 19:15:34 -0500

    [IMP] Add JWT to the app and require jwt to the get method on item resource
    <4.56>

--- src/main.py
+++ src/main.py
@@ -3,17 +3,22 @@ from os import environ
 from flask import Flask, request
 from flask_restful import Resource, Api
 from pdb import set_trace as debug
+from flask_jwt import JWT, jwt_required
 
+from security import authenticate, identity
 FLASK_PORT = environ.get('FLASK_PORT')
 
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity) # /auth endpoint created
+
 items = []
 
 class Item(Resource):
 
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x.get('name') == name, items), None)
         return {'item': item}, 200 if item else 404
@@ -38,4 +43,4 @@ class ItemList(Resource):
 if __name__ == '__main__':
     api.add_resource(Item, '/item/<string:name>')
     api.add_resource(ItemList, '/items')
-    app.run(port=FLASK_PORT, host='0.0.0.0')
+    app.run(port=FLASK_PORT, host='0.0.0.0', debug=True)
