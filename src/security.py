3b2e90e20b6662aa74321229f76e57ac98eb0729
ozrlz <oscar.cucei@gmail.com>
2018-07-25 13:53:49 -0500

    [REF] Change the static way to store users to the sqlite way
    <5.66>

--- src/security.py
+++ src/security.py
@@ -2,18 +2,12 @@ from werkzeug.security import safe_str_cmp
 from user import User
 from pdb import set_trace as debug
 
-users = [
-    User(1, 'bob', 'asd')
-]
-
-username_mapping = { u.username: u for u in users }
-userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, passwd):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.passwd, passwd):
         return user
 
 def identity(payload):
     user_id = payload.get('identity')
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
--- src/user.py
+++ src/user.py
@@ -1,5 +1,37 @@
+import sqlite3
+from os import environ
+
+DATABASE_NAME = environ.get('DATABASE_NAME')
+
 class User:
     def __init__(self, _id, username, passwd):
         self.id = _id
         self.username = username
         self.passwd = passwd
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect(DATABASE_NAME)
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username = ?"
+        result = cursor.execute(query, (username,) )
+        row = result.fetchone()
+        user = cls(*row) if row else None
+        
+        connection.close()
+
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect(DATABASE_NAME)
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id = ?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        user = cls(*row) if row else None
+
+        connection.close()
+        return user
