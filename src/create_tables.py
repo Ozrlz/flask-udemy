524a90ff5b611efda5c70b428502e7ceecd2af9d
ozrlz <oscar.cucei@gmail.com>
2018-07-26 09:48:53 -0500

    [ADD] Create a new table items and add a new row to it

--- src/create_tables.py
+++ src/create_tables.py
@@ -7,8 +7,13 @@ DATABASE_NAME = environ.get('DATABASE_NAME')
 con = sqlite3.connect(DATABASE_NAME)
 cr = con.cursor()
 
-create_table_query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
-cr.execute(create_table_query)
+create_table_users_query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+cr.execute(create_table_users_query)
+
+create_table_items_query = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cr.execute(create_table_items_query)
+cr.execute('INSERT INTO items VALUES (?,?)', ('piano', 10.021))
+
 
 con.commit()
 con.close()
