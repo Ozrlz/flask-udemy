64fec13c090a59aca99e7b6538aa27d63827a60c
ozrlz <oscar.cucei@gmail.com>
2018-07-30 10:45:54 -0500

    [IMP] Add a id column to the items table

--- src/create_tables.py
+++ src/create_tables.py
@@ -10,7 +10,7 @@ cr = con.cursor()
 create_table_users_query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cr.execute(create_table_users_query)
 
-create_table_items_query = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+create_table_items_query = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)"
 cr.execute(create_table_items_query)
 
 con.commit()
