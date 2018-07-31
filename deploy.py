108bf13c63f4b47c7e9c73c794d74fec0ea176cf
ozrlz <oscar.cucei@gmail.com>
2018-07-31 16:54:04 -0500

    [ADD] Add a deploy file to run as main when deploying.
    <9.107>

---
+++ deploy.py
@@ -0,0 +1,10 @@
+from main import app
+from db import db
+
+db.init_app(app)
+
+# Create tables before the first requests gets dispatched
+@app.before_first_request
+def create_tables():
+    db.create_all() 
+    
