109221c581749320c6224b519411be04c6e077c7
ozrlz <oscar.cucei@gmail.com>
2018-07-31 10:35:02 -0500

    [FIX] Write the .json() call

--- src/models/store.py
+++ src/models/store.py
@@ -14,7 +14,7 @@ class StoreModel(db.Model):
     def json(self):
         return {
             'name': self.name,
-            'items': [item.json for item in  self.items.all()]
+            'items': [item.json() for item in  self.items.all()]
         }
 
     @classmethod
